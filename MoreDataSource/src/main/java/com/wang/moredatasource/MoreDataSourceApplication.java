24911df81332ded78b28d89bb957900984d714d3
wangshaoshuai <wangshaoshuai>
2021-04-12 10:38:38 +0800

    多数据源

---
+++ MoreDataSource/src/main/java/com/wang/moredatasource/MoreDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.wang.moredatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoreDataSourceApplication.class, args);
+    }
+
+}
