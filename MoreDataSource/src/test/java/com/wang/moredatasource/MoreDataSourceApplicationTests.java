79982994b1fc0d7533f205d3c5534116945317b6
wangshaoshuai <wangshaoshuai>
2021-04-13 16:58:29 +0800

    多数据源

---
+++ MoreDataSource/src/test/java/com/wang/moredatasource/MoreDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.moredatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreDataSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
